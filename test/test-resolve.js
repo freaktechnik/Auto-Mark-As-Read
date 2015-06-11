8852732bda40b4186385e735470da623e3e93ce3
Martin Giger <martin.a.giger@sunrise.ch>
2015-06-11 17:21:55 +0200

    Test if it resolves correctly

--- test/test-resolve.js
+++ test/test-resolve.js
@@ -9,20 +9,38 @@
  */
  
 const { resolve, resolveURLIfNeeded } = require("../lib/resolve");
-const { Promise: sdkPromise } = require("sdk/core/promise");
+const { Promise: sdkPromise, all } = require("sdk/core/promise");
 
-const EXAMPLE_URL = "http://humanoids.be";
-const FEEDBURNER_EXAMPLE_URL = "http://feedproxy.google.com/~androidpolice";
+const EXAMPLES = {
+    fixture: [
+        "http://humanoids.be/",
+        "http://hbsl.ch/",
+        // shouldn't use production redirects from feedburner, since it'll probably forge analytics.
+        "http://feedproxy.google.com/~r/AndroidPolice/~3/6KV6BiUCLko/story01.htm"
+    ],
+    result: [
+        "http://humanoids.be/",
+        "http://humanoids.be/",
+        "http://www.androidpolice.com/2015/06/11/international-giveaway-win-one-of-100-airdroid-premium-membership-codes-from-airdroid-and-android-police/"
+    ]
+};
 
 exports["test resolve signature"] = (assert) => {
     assert.equal(typeof resolve, "function");
-    assert.ok(resolve(EXAMPLE_URL) instanceof sdkPromise);
+    EXAMPLES.fixture.forEach((url) => assert.ok(resolve(url) instanceof sdkPromise));
 };
 
 exports["test resolveURLIfNeeded signature"] = (assert) => {
     assert.equal(typeof resolveURLIfNeeded, "function");
-    assert.ok(resolveURLIfNeeded(EXAMPLE_URL) instanceof sdkPromise);
-    assert.ok(resolveURLIfNeeded(FEEDBURNER_EXAMPLE_URL) instanceof sdkPromise);
+    EXAMPLES.fixture.forEach((url) => assert.ok(resolveURLIfNeeded(url) instanceof sdkPromise));
+};
+
+exports["test resolve"] = (assert, done) => {
+    all(EXAMPLES.fixture.map((fixture, i) => {
+        return resolve(fixture).then((url) => {
+            return assert.equal(url, EXAMPLES.result[i]);
+        });
+    })).then(done);
 };
 
 require("sdk/test").run(exports);
